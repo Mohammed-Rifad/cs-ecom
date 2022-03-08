248b13fc7299eb65fde49f6224921df111beb193
Mohammed-Rifad <mohammedrifad17@gmail.com>
2022-03-08 15:20:20 +0530

    database

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +154,6 @@ EMAIL_HOST_PASSWORD = 'jinna216'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
