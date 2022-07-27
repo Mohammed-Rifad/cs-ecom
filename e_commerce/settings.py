5f6ab8484dfa7f1ca42f8c079f444e2b02360a75
Mohammed-Rifad <80377274+Mohammed-Rifad@users.noreply.github.com>
2022-07-27 09:04:07 +0530

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -150,7 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'mohammedrifad17@gmail.com'
-EMAIL_HOST_PASSWORD = 'jinna216'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
